b0984c90e0be56532ceb4abf570ccd38a1907211
dom-inic <dominicnyambane22@gmail.com>
2021-11-24 20:44:53 +0300

    adding new posts

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,16 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
 
-const initialState = [
-  { id: '0', name: 'elon musk ' },
-  { id: '1', name: 'Mack zuckerberg' },
-  { id: '2', name: 'Tim cook' }
-]
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get('/fakeApi/users')
+  return response.data
+})
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {
 
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload
+    })
   }
 })
 
